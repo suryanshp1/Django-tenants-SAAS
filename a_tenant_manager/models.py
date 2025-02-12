a4d299856971d438426ef5e6382dd10924f9a2e0
suryanshp1 <suryanshp1@gmail.com>
2025-02-12 14:46:13 +0530

    feat: Added auto add and drop schema

--- a_tenant_manager/models.py
+++ a_tenant_manager/models.py
@@ -6,6 +6,8 @@ class Tenant(TenantMixin):
     name = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    auto_create_schema = True
+    auto_drop_schema = True
 
 class Domain(DomainMixin):
     pass
